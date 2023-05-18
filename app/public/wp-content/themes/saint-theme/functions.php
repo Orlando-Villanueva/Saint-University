73b5f910c4dd0549253fc86e2184bf74309639f5
OrlandoAriel <orlandovillanueva11@gmail.com>
2023-05-17 22:39:31 -0400

    HTML template and links

--- app/public/wp-content/themes/saint-theme/functions.php
+++ app/public/wp-content/themes/saint-theme/functions.php
@@ -2,7 +2,18 @@
 
 function saint_files()
 {
-    wp_enqueue_style('saint_main_styles', get_stylesheet_uri());
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_style('saint_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('saint_extra_styles', get_theme_file_uri('/build/index.css'));
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 }
 
 add_action('wp_enqueue_scripts', 'saint_files');
+
+function saint_features()
+{
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'saint_features');
