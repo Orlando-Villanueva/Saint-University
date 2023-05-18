24d4ec6fb7a13017badd96fedd65e0d610a786db
OrlandoAriel <orlandovillanueva11@gmail.com>
2023-05-18 17:47:05 -0400

    Update page.php

--- app/public/wp-content/themes/saint-theme/page.php
+++ app/public/wp-content/themes/saint-theme/page.php
@@ -15,12 +15,21 @@ while (have_posts()) {
 </div>
 
 <div class="container container--narrow page-section">
+    <?php
+        $parentID = wp_get_post_parent_id(get_the_ID());
+        if ($parentID) {
+        ?>
     <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-            <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About
-                Us</a> <span class="metabox__main">Our History</span>
+            <a class="metabox__blog-home-link" href="<?php echo get_permalink($parentID) ?>">
+                <i class="fa fa-home" aria-hidden="true"></i>
+                Back to <?php echo get_the_title($parentID)  ?>
+            </a>
+            <span class="metabox__main"><?php the_title() ?> </span>
         </p>
     </div>
+    <?php } ?>
+
 
     <!-- <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
