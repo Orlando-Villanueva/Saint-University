7708e093daae9842c6a5bbc61cd4af55c30898b4
OrlandoAriel <orlandovillanueva11@gmail.com>
2023-05-19 00:04:17 -0400

    Update page.php

--- app/public/wp-content/themes/saint-theme/page.php
+++ app/public/wp-content/themes/saint-theme/page.php
@@ -1,48 +1,58 @@
 <?php
 get_header();
+$parentID = wp_get_post_parent_id(get_the_ID());
 
 while (have_posts()) {
     the_post(); ?>
-<div class="page-banner">
-    <div class="page-banner__bg-image"
-        style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title() ?></h1>
-        <div class="page-banner__intro">
-            <p>Don't forget to replace me later.</p>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title() ?></h1>
+            <div class="page-banner__intro">
+                <p>Don't forget to replace me later.</p>
+            </div>
         </div>
     </div>
-</div>
 
-<div class="container container--narrow page-section">
-    <?php
-        $parentID = wp_get_post_parent_id(get_the_ID());
-        if ($parentID) {
+    <div class="container container--narrow page-section">
+        <?php if ($parentID) { ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($parentID) ?>">
+                        <i class="fa fa-home" aria-hidden="true"></i>
+                        Back to <?php echo get_the_title($parentID)  ?>
+                    </a>
+                    <span class="metabox__main"><?php the_title() ?> </span>
+                </p>
+            </div>
+        <?php } ?>
+
+        <?php
+        $test = get_pages(array('child_of' => get_the_ID()));
+        if ($parentID || $test) {
         ?>
-    <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-            <a class="metabox__blog-home-link" href="<?php echo get_permalink($parentID) ?>">
-                <i class="fa fa-home" aria-hidden="true"></i>
-                Back to <?php echo get_the_title($parentID)  ?>
-            </a>
-            <span class="metabox__main"><?php the_title() ?> </span>
-        </p>
+            <div class="page-links">
+                <h2 class="page-links__title">
+                    <a href="<?php echo get_permalink($parentID) ?>">
+                        <?php echo get_the_title($parentID) ?>
+                    </a>
+                </h2>
+                <ul class="min-list">
+                    <?php
+                    wp_list_pages(array(
+                        'title_li' => NULL,
+                        'child_of' => $parentID ? $parentID : get_the_ID()
+                    ));
+                    ?>
+                </ul>
+            </div>
+
+        <?php } ?>
+
+        <div class="generic-content">
+            <?php the_content() ?>
+        </div>
     </div>
-    <?php } ?>
-
-
-    <!-- <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
-        <ul class="min-list">
-            <li class="current_page_item"><a href="#">Our History</a></li>
-            <li><a href="#">Our Goals</a></li>
-        </ul>
-    </div> -->
-
-    <div class="generic-content">
-        <?php the_content() ?>
-    </div>
-</div>
 <?php
 }
 get_footer();
